20ea2fc8b64a542d6caefd5ce0f9b584abd84f21
azrafe7 <azrafe7@gmail.com>
2017-11-11 20:23:45 +0100

    BOOL_DEFINES, optional prefix, comment traces, printAll()

--- src/booldefs/Defines.hx
+++ src/booldefs/Defines.hx
@@ -1,16 +1,27 @@
 package booldefs;
 
+import haxe.macro.Compiler;
+import haxe.macro.Context;
+
 
 class Defines {
   
+  public static var BOOL_DEFINES = []; // this stores the names of the fixed defines
+  
+  static function __init__():Void {
+    Compiler.define("BOOL_DEFINES");
+    haxe.macro.Context.warning("Using BOOL_DEFINES", Context.currentPos());
+  }
+  
   macro static public function asBool(define:String) {
-    var boolDefines = haxe.macro.Context.definedValue("FIX_BOOL_DEFINES");
+    var boolDefines = haxe.macro.Context.definedValue("BOOL_DEFINES");
     if (boolDefines == null || boolDefines.split(",").indexOf(define) < 0) {
-      haxe.macro.Context.fatalError("Cannot use `asBool('" + define + "')`.\nTry compiling with `--macro booldefs.Defines.fixBoolDefines(['" + define + "'])`", haxe.macro.Context.currentPos());
+      haxe.macro.Context.fatalError("ERROR: Cannot use `asBool('" + define + "')`.\n" + 
+        "Try compiling with `--macro booldefs.Defines.fixBoolDefines(['" + define + "'])`", haxe.macro.Context.currentPos());
     }
     var v = haxe.macro.Context.definedValue(define);
     var isTrue = !(v == null || v == "" || v == "0" || v == "false");
-    trace("asBool(" + define + "='" + v + "'): " + isTrue);
+    //trace("asBool(" + define + "='" + v + "'): " + isTrue);
     if (isTrue) {
       return macro true;
     } else return macro false;
@@ -20,7 +31,7 @@ class Defines {
   static function __asBool(define:String) {
     var v = haxe.macro.Context.definedValue(define);
     var isTrue = !(v == null || v == "" || v == "0" || v == "false");
-    trace("__asBool(" + define + "='" + v + "'): " + isTrue);
+    //trace("__asBool(" + define + "='" + v + "'): " + isTrue);
     if (isTrue) {
       return true;
     } else return false;
@@ -28,27 +39,35 @@ class Defines {
   #end
 
   #if macro
-  static var FIX_BOOL_DEFINES = [];
-  static public function fixBoolDefines(defines:Array<String>) {
-    if (FIX_BOOL_DEFINES.length == 0) {
-      trace("ENABLE FIX_BOOL_DEFINES");
-      haxe.macro.Compiler.define("FIX_BOOL_DEFINES", "1");
-      FIX_BOOL_DEFINES = ["1"];
+  static public function fixBoolDefines(defines:Array<String>, ?prefix:String) {
+    //trace("fixBoolDefines");
+    if (BOOL_DEFINES.length == 0) {
+      //trace("ENABLE BOOL_DEFINES");
+      haxe.macro.Compiler.define("BOOL_DEFINES", "1");
+      BOOL_DEFINES = ["1"];
     }
-    FIX_BOOL_DEFINES = FIX_BOOL_DEFINES.concat(defines);
-    haxe.macro.Compiler.define("FIX_BOOL_DEFINES", FIX_BOOL_DEFINES.join(","));
+    BOOL_DEFINES = BOOL_DEFINES.concat(defines);
+    haxe.macro.Compiler.define("BOOL_DEFINES", BOOL_DEFINES.join(","));
     
     for (define in defines) {
       var isTrue = __asBool(define);
-      trace(define + ": " + isTrue);
+      //trace(define + ": " + isTrue);
       
-      if (!isTrue) {
-        trace('  FALSE (redefined to "")');
-        haxe.macro.Compiler.define(define, "");
-      } else {
-        trace("  TRUE");
+      if (prefix != null) // create new define with prefix
+      {
+        haxe.macro.Compiler.define(prefix + define, isTrue ? "true" : "false");
+      } 
+      else // overwrite existing define
+      { 
+        if (!isTrue) {
+          //trace('  FALSE (redefined to "")');
+          haxe.macro.Compiler.define(define, "");
+        } else {
+          //trace("  TRUE");
+        }
       }
     }
+    
     return null;
   }
   #end
@@ -64,5 +83,15 @@ class Defines {
     }
     var str = buf.toString();
     return macro $v{str};
+  }
+  
+  #if !macro macro #end
+  static public function printAll() {
+    var mStr = allAsLogString();
+    switch (mStr.expr) {
+      case EConst(CString(str)): trace(str);
+      case _:
+    }
+    return null;
   }
 }
